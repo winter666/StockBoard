1bb53f7c941d9ba5cb6bc33a4b2fbf7dcdeb8589
Davut <sergeevdavut@mail.ru>
2022-06-29 20:17:32 +0300

    added string helpers

--- resources/js/vendor/helpers.js
+++ resources/js/vendor/helpers.js
@@ -10,4 +10,37 @@ export default {
 
     return `<img src="${url}" alt="${alt}" style="width: ${width}; margin: 0 5px;"/>`;
   },
+
+  string: {
+    ru_chars: {
+      а: 'a', б: 'b', в: 'v', г: 'g',
+      д: 'd', е: 'e', ё: 'yo', ж: 'zh',
+      з: 'z', и: 'i', й: 'i', к: 'k',
+      л: 'l', м: 'm', н: 'n', о: 'o',
+      п: 'p', р: 'r', с: 's', т: 't',
+      у: 'u', ф: 'f', х: 'h', ц: 'c',
+      ч: 'ch', ш: 'sh', щ: 'sch',
+      ы: 'y', э: 'e', ю: 'yu', я: 'ya',
+    },
+    random(length = 16) {
+      return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, length);
+    },
+    slug(str) {
+      const target = str.replace(/\s+/g, '-').toLowerCase();
+      const str_contain = target.split('');
+      let str_result = '';
+      const eng_chars = 'abcdefghigklmnopqrstuvwxyz'.split('');
+      str_contain.forEach((char) => {
+        if (this.ru_chars.hasOwnProperty(char)) {
+          str_result += this.ru_chars[char];
+        } else if (eng_chars.includes(char)) {
+          str_result += char;
+        } else if (char === '-') {
+          str_result += char;
+        }
+      });
+
+      return str_result;
+    },
+  },
 };
