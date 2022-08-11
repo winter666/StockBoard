f8c7da06f5b695d15bc283b47b1878ebd711ae3d
Davut <sergeevdavut@mail.ru>
2022-08-12 01:47:07 +0300

    Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(SyncBlockchainCommand::class)->everyFifteenMinutes();
+        $schedule->command(SyncBlockchainCommand::class)->everyThirtyMinutes();
     }
 
     /**
